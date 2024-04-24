8d8532c7178993097d490dbe8baf7c5086ba2d3d
bigguysahaj <sahajssingh2001@gmail.com>
2024-04-24 18:55:03 +0530

    stating point

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+ import Link from "next/link";
+
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-r from-rose-100 to-teal-100 text-gray text-xl font-bolder">
+      <span className=" ">Hey we getting started</span>
+    </main>
+  );
+}
